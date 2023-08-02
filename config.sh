e860a63bebeaaed9a8de01d3ddb9d936d7332ee5
Hotorou <37245252+ZunayedDihan@users.noreply.github.com>
2023-08-02 22:04:00 +0600

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 export TWRP_MANIFEST=https://github.com/MT6781/manifest_pb
 export TWRP_BRANCH=android-12.1
-export DT_LINK=https://github.com/MT6781/android_device_xiaomi_fleur-twrp
-export DT_BRANCH=android-12.1
-export DEVICE_PATH=device/xiaomi/fleur
-export DEVICE=fleur
-export MAKEFILE=omni_fleur
+export DT_LINK=https://github.com/ZunayedDihan/android_device_infinix_X6827
+export DT_BRANCH=4.0
+export DEVICE_PATH=device/infinix/X6827
+export DEVICE=X6827
+export MAKEFILE=omni_X6827
 export TARGET=pbrpimage
 export OUTPUT=pbrp.img
 export ALT_FILE=P*.zip

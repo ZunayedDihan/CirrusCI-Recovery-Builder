d17ef7a5c0e433a88ff7677e8985cec704126f37
ZunayedDihan <zunayeddihan@gmail.com>
2023-09-21 14:46:03 +0600

    update

--- config.sh
+++ config.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-export TWRP_MANIFEST=https://github.com/MT6781/manifest_pb
-export TWRP_BRANCH=android-12.1
+export TWRP_MANIFEST=https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp
+export TWRP_BRANCH=twrp-11
 export DT_LINK=https://github.com/ZunayedDihan/android_device_infinix_X6827
 export DT_BRANCH=4.0
 export DEVICE_PATH=device/infinix/X6827
 export DEVICE=X6827
-export MAKEFILE=omni_X6827
-export TARGET=pbrpimage
-export OUTPUT=pbrp.img
-export ALT_FILE=P*.zip
-export RANDOM=32427
+export MAKEFILE=twrp_X6827
+export TARGET=bootimage
+export OUTPUT=boot.img

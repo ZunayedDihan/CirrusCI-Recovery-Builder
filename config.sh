adadb59e4e908f8ed1af08192536695d7e9caef4
Hotorou <37245252+ZunayedDihan@users.noreply.github.com>
2023-09-22 00:24:05 +0600

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 export TWRP_MANIFEST=https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp
-export TWRP_BRANCH=twrp-11
+export TWRP_BRANCH=twrp-12.1
 export DT_LINK=https://github.com/ZunayedDihan/android_device_infinix_X6827
-export DT_BRANCH=4.0
+export DT_BRANCH=twrp-12
 export DEVICE_PATH=device/infinix/X6827
 export DEVICE=X6827
 export MAKEFILE=twrp_X6827
-export TARGET=bootimage
-export OUTPUT=boot.img
+export TARGET=vendorbootimage
+export OUTPUT=vendor_boot.img

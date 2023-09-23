8da130ae84791689c3e225c33c686855e54024a9
Hotorou <37245252+ZunayedDihan@users.noreply.github.com>
2023-09-24 00:18:17 +0600

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 export TWRP_MANIFEST=https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp
 export TWRP_BRANCH=twrp-11
 export DT_LINK=https://github.com/ZunayedDihan/android_device_infinix_X6827
-export DT_BRANCH=4.0
+export DT_BRANCH=tem
 export DEVICE_PATH=device/infinix/X6827
 export DEVICE=X6827
 export MAKEFILE=twrp_X6827
